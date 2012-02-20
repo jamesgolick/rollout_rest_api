5f91581ad1306528488d5972e28d4aa8fc8b6ffb
James Golick <jamesgolick@gmail.com>
2012-02-19 21:35:36 -0800

    basics

---
+++ lib/rollout_rest_api.rb
@@ -0,0 +1,4 @@
+require "sinatra"
+
+class RolloutRestAPI < Sinatra::Base
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,11 +2,14 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
 require 'rollout_rest_api'
+require 'rack/test'
+
+set :environment, :test
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |config|
-  
+  config.include Rack::Test::Methods
 end
